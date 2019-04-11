0e646d1b1c2cdf0e1e7d5117d88fe6dff60bbe55
Baldeep Singh Sidhu <726755@student.tor.loc>
2019-04-11 16:58:34 -0400

    Section B C D Commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,17 +16,23 @@ namespace ConsoleApp1
         {
             Program p = new Program();
             p.Beowulf = new ArrayList();
+            p.Run(); //Section A
+            string lines = System.IO.File.ReadAllText("U:/Users/726755.STUDENT/balli/beowulf.txt");
+            p.FindNumberOfBlankSpaces(lines); //(Section B )
+            p.ProcessArrayList();
+
         }
         public void Run() { this.ReadTextFiles(); }
         public void ReadTextFiles()
         {
-            using (StreamReader file = new StreamReader("U:/balli/beowulf.txt"))
+            using (StreamReader file = new StreamReader("U:/Users/726755.STUDENT/balli/beowulf.txt"))
             {
                 int counter = 0;
                 string ln;
                 while ((ln = file.ReadLine()) != null)
                 {
                     Console.WriteLine(ln);
+                    counter++;
                     Beowulf.Add(ln);
 
                 }
@@ -40,12 +46,41 @@ namespace ConsoleApp1
             int countSpaces = 0;
             foreach (char c in line)
             {
-                if (char.IsLetter(c)) { countletters++; }
-                if (char.IsWhiteSpace(c)) { countletters++; }
+                if (char.IsLetter(c))
+                {
+                    countletters++;
+                }
+                if (char.IsWhiteSpace(c)) { countSpaces++; }
+                Console.WriteLine($"File has {countSpaces} Blank Spaces.");
+                Console.WriteLine($"File has {countletters} Words.");
             }
             return countSpaces;
         }
+        public void ProcessArrayList()
+        {
+            int LineNumber = 0;
+            foreach (var line in Beowulf)
+
+            {
+
+                if (!ContainWord(line.ToString().ToLower(), "war") && ContainWord(line.ToString().ToLower(), "fare"))
+                {
+                    Console.WriteLine(line);
+                    Console.WriteLine("Line number is {0}", LineNumber);
+                    LineNumber++;
+                }
+            }
+            Console.WriteLine(LineNumber);
+        }
+        public bool ContainWord(string line, string Word)
+        {
+            if (line.Contains(Word) == true)
+            {
+                return true;
+            }
+            return false;
+        }
+
 
     }
 }
-
